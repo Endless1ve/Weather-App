42b6296f832ed54e19285b8a0f747221f566daab
Artem Tulupov <stalin.dignitas70@gmail.com>
2023-12-08 20:24:40 +0300

    add casds creation

--- src/scripts/components/renderWeekly/renderWeekly.js
+++ src/scripts/components/renderWeekly/renderWeekly.js
@@ -1,3 +1,14 @@
+import { weeklyContainer } from "../../variables";
+import { createWeeklyCard } from "../createWeeklyCard/createWeeklyCard";
+
 export function renderWeekly(data) {
-  // data.then((data) => console.log(JSON.stringify(data)));
+  data
+    .then((data) => data.data)
+    .then((res) => res.filter((item) => new Date(item.time).getHours() === 11))
+    .then((res) =>
+      res.forEach((element) => {
+        const card = createWeeklyCard(element);
+        weeklyContainer.insertAdjacentHTML("beforeend", card);
+      })
+    );
 }
