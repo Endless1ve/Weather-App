52e3fef569e8d2d676e74fda76d6f6e869cb37b5
Artem Tulupov <stalin.dignitas70@gmail.com>
2023-12-11 18:45:18 +0300

    fix error styles, add progress bar and error logic

--- src/index.js
+++ src/index.js
@@ -2,22 +2,29 @@ import "./index.css";
 
 const displayErrorBtn = document.querySelector(".display");
 const error = document.querySelector(".error");
-const closeError = document.querySelector(".errorClose");
-const bar = document.querySelector(".progress");
-
-displayErrorBtn.addEventListener("click", (e) => {
+const closeErrorBtn = document.querySelector(".errorClose");
+const bar = document.querySelector(".errorProgress");
+function displayError() {
   error.classList.add("errorVisible");
-  setTimeout(() => {
-    error.classList.remove("errorVisible");
-  }, 10000);
-});
+  let progress = 0;
+  const increment = 100 / ((10 * 1000) / 50);
+  const interval = setInterval(() => {
+    progress += increment;
+    bar.style.width = progress + "%";
+    if (progress >= 106 || !error.classList.contains("errorVisible")) {
+      clearInterval(interval);
+      closeError();
+    }
+  }, 50);
+  closeErrorBtn.addEventListener("click", closeError);
+}
 
-closeError.addEventListener("click", (e) => {
+function closeError() {
   error.classList.remove("errorVisible");
-});
+  closeErrorBtn.removeEventListener("click", closeError);
+  setTimeout(() => {
+    bar.style.width = "0";
+  }, 500);
+}
 
-setInterval(() => {
-  const width = bar.style.width;
-  console.log(width);
-  bar.style.width += width + 10 + "%";
-}, 1000);
+displayErrorBtn.addEventListener("click", displayError);
