c3a9ebce5fad2786cdd04f41e2fdaa5bf4910864
Artem Tulupov <stalin.dignitas70@gmail.com>
2023-12-11 13:21:22 +0300

    added error logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import "./index.css";
+
+const displayErrorBtn = document.querySelector(".display");
+const error = document.querySelector(".error");
+const closeError = document.querySelector(".errorClose");
+const bar = document.querySelector(".progress");
+
+displayErrorBtn.addEventListener("click", (e) => {
+  error.classList.add("errorVisible");
+  setTimeout(() => {
+    error.classList.remove("errorVisible");
+  }, 10000);
+});
+
+closeError.addEventListener("click", (e) => {
+  error.classList.remove("errorVisible");
+});
+
+setInterval(() => {
+  const width = bar.style.width;
+  console.log(width);
+  bar.style.width += width + 10 + "%";
+}, 1000);
