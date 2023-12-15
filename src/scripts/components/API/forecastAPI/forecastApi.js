1c04dd05b0a865c8afd460cd6234f2a54760feae
Artem Tulupov <stalin.dignitas70@gmail.com>
2023-12-15 17:19:23 +0300

    create handler render funct

--- src/scripts/components/API/forecastAPI/forecastApi.js
+++ src/scripts/components/API/forecastAPI/forecastApi.js
@@ -1,9 +1,10 @@
+import { handler } from "../../../..";
 import { API_KEY, main } from "../../../variables";
 import { deletePreloader } from "../../preloader/deletePreloader";
 import { removePlug } from "../../removePlug/removePlug";
-import { dailyHandler, renderDaily } from "../../renderDaily/renderDaily";
-import { hourlyHandler, renderHourly } from "../../renderHourly/renderHourly";
-import { renderWeekly, weeklyHandler } from "../../renderWeekly/renderWeekly";
+import { renderDaily } from "../../renderDaily/renderDaily";
+import { handlerHourly } from "../../handlerHourly/handlerHourly";
+import { handlerWeekly } from "../../handlerWeekly/handlerWeekly";
 import { getDaysApi } from "../dateApi/dateApi";
 
 export function getForecasts(latitude, longitude) {
@@ -11,7 +12,7 @@ export function getForecasts(latitude, longitude) {
   const urls = [
     {
       link: `https://meteostat.p.rapidapi.com/point/hourly?lat=${latitude}&lon=${longitude}&start=${nextDay}&end=${nextWeekDay}`,
-      funName: weeklyHandler,
+      renderFun: handlerWeekly,
       headers: {
         "X-RapidAPI-Key": "afc8a65e30msh735c1f0c55d4ab9p129605jsn48c2e1f0afc1",
         "X-RapidAPI-Host": "meteostat.p.rapidapi.com",
@@ -19,7 +20,7 @@ export function getForecasts(latitude, longitude) {
     },
     {
       link: `https://meteostat.p.rapidapi.com/point/hourly?lat=${latitude}&lon=${longitude}&start=${currDay}&end=${nextDay}`,
-      funName: hourlyHandler,
+      renderFun: handlerHourly,
       headers: {
         "X-RapidAPI-Key": "afc8a65e30msh735c1f0c55d4ab9p129605jsn48c2e1f0afc1",
         "X-RapidAPI-Host": "meteostat.p.rapidapi.com",
@@ -27,35 +28,38 @@ export function getForecasts(latitude, longitude) {
     },
     {
       link: `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric&lang=ru`,
-      funName: dailyHandler,
+      renderFun: renderDaily,
     },
   ];
   const mockUrls = [
     {
       link: `http://localhost:5000/weekly`,
-      funName: weeklyHandler,
+      renderFun: handlerWeekly,
     },
     {
       link: `http://localhost:5000/hourly`,
-      funName: hourlyHandler,
+      renderFun: handlerHourly,
     },
     {
       link: `http://localhost:5000/daily`,
-      funName: dailyHandler,
+      renderFun: renderDaily,
     },
   ];
 
-  const responses = mockUrls.map((url) => {
-    url.link = fetch(url.link, {
-      headers: url.headers,
+  const responses = mockUrls.map((item) => {
+    return fetch(item.link, {
+      headers: item.headers,
     })
       .then((res) => res.json())
+      .then((data) => ({
+        data,
+        renderFun: item.renderFun,
+      }))
       .catch((err) => console.log(err));
-    return url;
   });
 
   Promise.all(responses)
-    .then((res) => res.forEach((item) => item.funName(item.link)))
+    .then((res) => res.forEach((item) => item.renderFun(item.data)))
     .then(() => removePlug())
     .then(() => (main.style.display = "block"))
     .catch((err) => console.log(err))
