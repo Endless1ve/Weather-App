dd72a4c9d6c06bc21d98b00411dfe7e2c302ca24
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-01-10 16:35:49 +0300

    Updates

--- main.b3d07302a9a8891ad6e6.js
+++ main.b3d07302a9a8891ad6e6.js
@@ -4437,7 +4437,7 @@ function getForecasts(latitude, longitude) {
     link: "http://localhost:5000/daily",
     renderFun: handlerDaily
   }];
-  var responses = mockUrls.map(function (item) {
+  var responses = urls.map(function (item) {
     return fetch(item.link, {
       headers: item.headers
     }).then(function (res) {
