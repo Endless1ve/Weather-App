30d3a4efcd1b2c559cb10b70130f3430a82c8ecc
Artem Tulupov <stalin.dignitas70@gmail.com>
2024-03-20 13:34:47 +0300

    fix formatting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./index.css";
 import { scrollingBlocks } from "./scripts/components/UI/scrollingBlocks/scrollingBlocks";
-import { toggleTheme } from "./scripts/components/UI/toggleTheme/toggleTheme";
 import { getLocation } from "./scripts/components/getLocation/getLocation";
 
 const scrollBlocks = document.querySelectorAll("#scroll");
